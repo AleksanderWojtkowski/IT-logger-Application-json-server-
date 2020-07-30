727bab4eebaa6a59c299f758df4d209a5bfe1fdb
AleksanderWojtkowski <aleksanderwojtkowski@gmail.com>
2020-07-30 18:30:19 +0200

    ready to implement Redux and minify hooks usage

---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export const TechItem = ({tech}) => {
+    return (
+        <li className='collection-item'>
+            <div>
+                {tech.firstName} {tech.lastName}
+                <a href="#!" className='secondary-content'>
+                    <i className="material-icons grey-text">delete</i>
+                </a>
+            </div>
+            
+        </li>
+    )
+}
+
+TechItem.propTypes = {
+    prop: PropTypes.object.isRequired
+}
+
+
+
+export default TechItem
